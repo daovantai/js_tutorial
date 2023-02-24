7f15fda3add39331b8d89891326720fd88111219
daovantai <daotai2309@gmail.com>
2023-02-24 10:41:05 +0700

    first commit

---
+++ hello/hello.js
@@ -0,0 +1,11 @@
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "nhupham";
+
+    document.getElementById("demo2").innerHTML = "vkl luon";
+}
+function myFunction2() {
+    document.getElementById("demo3").innerHTML = "test3";
+
+    document.getElementById("demo4").innerHTML = "test872387";
+}
