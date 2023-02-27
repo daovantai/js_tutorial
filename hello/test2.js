4f0b0caba6d8f97f01d3d5793b6f0e866b381cc1
daovantai <daotai2309@gmail.com>
2023-02-27 10:29:42 +0700

    update js

---
+++ hello/test2.js
@@ -0,0 +1,78 @@
+// let a=5;
+// let b=5.7;
+// let c="I'm TB King"
+// let d= "dao van tainhu   "
+// console.log(`ok daovantai ${c+d}`);
+// console.log(d.split())
+// console.log(d.toUpperCase())
+// console.log(`urf:/api/tb/${a}`)
+
+// document.write(`ok ${d}`, 'hahaha')
+
+// for (let i=0; i<5; i++){
+//     console.log(`${i}taibu`)
+// }
+
+let cars= ['mec','bmw','lambo','vkl','bmw22']
+// cars.forEach(function(car){
+//     console.log(car)
+// })
+
+// cars.forEach((car) => {
+//     console.log(car)
+// })
+
+// for (const ok of cars) {
+//     console.log(`name:${ok}`)
+// }
+
+// cars.forEach((car,index,arr) =>{
+//     console.log(`i =${index} car= ${car}`)
+// })
+
+// cars.push("vario")
+// let filterCar = cars.filter(function(car) {
+//     return car.toLowerCase().includes("bmw")
+// })
+// console.log(filterCar.length)
+// console.log(`filterCar= ${filterCar}`)   
+
+let someNumbers= [3,5,7,1,5,9]
+// let squareNumbers= someNumbers.map(eachNumber => {
+//     return eachNumber * eachNumber
+// })
+// console.log(`squareNumbers= ${squareNumbers}`)
+
+// console.log(squareNumbers)
+
+// someNumbers= someNumbers.filter(eachNumber => 
+//     eachNumber >=5
+// )
+// console.log(someNumbers)
+
+
+// function sayHello(name, dc){
+//     console.log(`Hello ${name} ${dc}`)
+// }
+
+// sayHello(8,"ad")
+
+
+// const multiply= function(a,b){
+//     return a*b
+// }
+// console.log(`multiply ${multiply(4,6)}`)
+
+// let divide= (a,b) => (a/b).toFixed(2)
+// console.log(divide(9,7))
+
+// let s=0
+// setInterval(function(){
+//     s+=1
+//     console.log(`${s} call Tb`)
+// },1000)
+
+let point={}
+point.tb="haha"
+
+console.log(point.tb)
