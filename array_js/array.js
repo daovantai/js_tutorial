66729c2fd41714d87399ae124723e1f952d4375d
daovantai <daotai2309@gmail.com>
2023-03-02 14:25:30 +0700

    update

---
+++ array_js/array.js
@@ -0,0 +1,21 @@
+console.log("array in js");
+let a = {
+  name: "ptit",
+  age: 50,
+};
+
+console.log("check data: ", a);
+console.log(`my name's :`, a.name);
+
+let b = ["MU", 123, a];
+console.log(b[0]);
+console.log(b[1]);
+console.log(b[2]);
+console.log(typeof b, b);
+
+let user = [
+  { name: "tb", age: 21, tr: "ptit" },
+  { name: "nhupham", age: 18, tr: "thanglong" },
+  { name: "diem", age: 19, tr: "congdoan" },
+];
+console.log(user);
