40b8f612cc54f84050c1d700ccaa84d06dc8faae
daovantai <daotai2309@gmail.com>
2023-03-02 14:26:58 +0700

    js

---
+++ object_js/object.js
@@ -0,0 +1,12 @@
+console.log("ok run");
+let user = {
+  name: "tb",
+  age: 21,
+  ny: "nhupham",
+};
+
+let b = "name";
+user[b] = "diem";
+console.log(`what crush:`, user.name);
+
+console.log(user["ny"]);
---
+++ strings_js/test.js
@@ -0,0 +1,12 @@
+console.log("vkl");
+
+let a = 5;
+
+let b = "5";
+
+console.log(a * b);
+console.log(a + b);
+console.log(a - b);
+console.log(a / b);
+
+console.log(a === b, typeof a, typeof b);
